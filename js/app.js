58c3fd264bf69e13e90071fc3208cc0e17d1115d
Vu Anh <brother.rain.1024@gmail.com>
2016-07-05 14:06:57 +0700

    add scala

--- js/app.js
+++ js/app.js
@@ -43,6 +43,12 @@ myApp.controller('MyController', function MyController($scope) {
       "tag": "training"
     },
     {
+      "name": "Scala",
+      "image": "https://d13yacurqjgara.cloudfront.net/users/210858/screenshots/2113044/code_logo_1x.jpg",
+      "url": "/training/scala",
+      "tag": "training"
+    },
+    {
       "name": "Big Data",
       "image": "https://d13yacurqjgara.cloudfront.net/users/210858/screenshots/2113044/code_logo_1x.jpg",
       "url": "/training/bigdata",
