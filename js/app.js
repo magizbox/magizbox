c3cae22cabfc10cc8b928f64a4d025b611cce6a5
Vu Anh <brother.rain.1024@gmail.com>
2016-07-05 14:19:33 +0700

    add octave

--- js/app.js
+++ js/app.js
@@ -55,6 +55,12 @@ myApp.controller('MyController', function MyController($scope) {
       "tag": "training"
     },
     {
+      "name": "Octave",
+      "image": "https://d13yacurqjgara.cloudfront.net/users/210858/screenshots/2113044/code_logo_1x.jpg",
+      "url": "/training/octave",
+      "tag": "training"
+    },
+    {
       "name": "Big Data",
       "image": "https://d13yacurqjgara.cloudfront.net/users/210858/screenshots/2113044/code_logo_1x.jpg",
       "url": "/training/bigdata",
